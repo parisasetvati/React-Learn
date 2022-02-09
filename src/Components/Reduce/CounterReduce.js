0f507fb63c0b9a9ca71dbf2d89088d82f8476d19
parisa setvati <p.s.setvati@gmail.com>
2022-02-09 20:01:42 +0330

    Counter Reduce change

--- src/Components/Reduce/CounterReduce.js
+++ src/Components/Reduce/CounterReduce.js
@@ -5,11 +5,17 @@ const Reducer=(state,action)=>{
     console.log(state,action);
     switch (action.type){
         case 'addOne':
-            return {firstCounter:state.firstCounter+1};
+            return {...state,firstCounter:state.firstCounter+action.value};
             case 'addFive':
-                return {firstCounter:state.firstCounter+5};
+                return {...state,firstCounter:state.firstCounter+action.value};
                 case 'decrement':
-                    return {firstCounter:state.firstCounter-1};
+                    return {...state,firstCounter:state.firstCounter-action.value};
+                    case 'addOne2':
+                        return {...state,secondCounter:state.secondCounter+action.value};
+                        case 'addFive2':
+                            return {...state,secondCounter:state.secondCounter+action.value};
+                            case 'decrement2':
+                                return {...state,secondCounter:state.secondCounter-action.value};
                     case 'reset':
                         return initialState;
                     default:
@@ -25,11 +31,26 @@ const CounterReduce = () => {
     return (
 
 <div>
-    <h2>count is {count.firstCounter}</h2>
-    <button onClick={()=>dispatch({type:"addOne"})}>addOne</button>
-    <button onClick={()=>dispatch({type:"addFive"})}>addFive</button>
-    <button onClick={()=>dispatch({type:"decrement"})}>decrement</button>
+<h2>count is {count.firstCounter}</h2>
+    <div>
+    <button onClick={()=>dispatch({type:"addOne",value:1})}>addOne</button>
+    <button onClick={()=>dispatch({type:"addFive",value:5})}>addFive</button>
+    <button onClick={()=>dispatch({type:"decrement",value:1})}>decrement</button>
+    </div>
+   
+   
+    <h2>count is {count.secondCounter}</h2>
+    <div>
+
+    <button onClick={()=>dispatch({type:"addOne2",value:1})}>addOne</button>
+    <button onClick={()=>dispatch({type:"addFive2",value:5})}>addFive</button>
+    <button onClick={()=>dispatch({type:"decrement2",value:1})}>decrement</button>
+   
+    </div>
+    <div>
     <button onClick={()=>dispatch({type:"reset"})}>reset</button>
+    </div>
+   
 </div>
       );
 }
