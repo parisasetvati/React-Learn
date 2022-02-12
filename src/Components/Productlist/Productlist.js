582510b2bb2cef9ff78cf657d7575cc9aba2147a
parisa setvati <p.s.setvati@gmail.com>
2022-02-12 09:13:19 +0330

    reducer added

--- src/Components/Productlist/Productlist.js
+++ src/Components/Productlist/Productlist.js
@@ -3,16 +3,16 @@ import React from "react";
 import { useProdocts, UseProductsAction } from "../Provider/ProductsProvider";
 const Productlist = (props) => {
     const products=useProdocts();
-    const {removeHandler,changeHandler,oncrementHandler,ondecrementHandler}=UseProductsAction();
+    const dispatch=UseProductsAction();
     return ( 
         <div>
         {products.map((product,index)=>{
 
           return <Product product={product}
-          onCrement={()=>oncrementHandler(product.id)} 
-          onDecrement={()=>ondecrementHandler(product.id)}
-          onchange={(e)=>changeHandler(e,product.id)}
-          onRemove={()=>removeHandler(product.id)}
+          onCrement={()=>dispatch({type:'crement',id:product.id})} 
+          onDecrement={()=>dispatch({type:'decrement',id:product.id})}
+          onchange={(e)=>dispatch({type:'edit',id:product.id,event:e})}
+          onRemove={()=>dispatch({type:'remove',id:product.id})}
            />
         })
     }
--- src/Components/Provider/ProductsProvider.js
+++ src/Components/Provider/ProductsProvider.js
@@ -1,16 +1,66 @@
-import React, { useContext ,useState} from "react";
-
+import React, { useContext ,useReducer} from "react";
+const initialState=([
+    {title:"React.js",price:"99$",quantity:1,id:1},
+    {title:"Node.js",price:"89$",quantity:2,id:2},
+    {title:"javasrt",price:"79$",quantity:3,id:3},
+]);
+const reducer=(state,action)=>{
+    console.log(state,action);
+    switch (action.type){
+     case 'crement':{
+        const index=state.findIndex((p)=>p.id===action.id);
+        const product={...state[index]};
+        product.quantity++;
+      const updateProducts=[...state];
+     updateProducts[index]=product;
+     return updateProducts;
+    
+     }
+     
+    
+    case 'decrement':
+    {
+        const index=state.findIndex((p)=>p.id===action.id);
+        
+        const product={...state[index]};
+        if (product.quantity===1){
+            const filterItem=state.filter((p)=>p.id!==action.id);
+            return filterItem;
+        }
+        else{
+        product.quantity--;
+      const updateProducts=[...state];
+     updateProducts[index]=product;
+    return updateProducts;
+        }
+    }
+       
+    case 'edit':{
+        const index=state.findIndex((item)=>item.id===action.id);
+        const product={...state[index]};
+        product.title=action.event.target.value;
+       
+        const  updateProducts=[...state];
+        updateProducts[index]=product;
+        return updateProducts;
+    
+    }
+       
+    case 'remove':{
+        const filterItem=state.filter((p)=>p.id!==action.id);
+        return filterItem;
+    }
+        
+    
+}
+}
 const ProductsContext=React.createContext();
 const ProductsContextDispacher=React.createContext();
 const ProductsProvider = ({children}) => {
-    const[products,setProducts]=useState([
-        {title:"React.js",price:"99$",quantity:1,id:1},
-        {title:"Node.js",price:"89$",quantity:2,id:2},
-        {title:"javasrt",price:"79$",quantity:3,id:3},
-    ]);
+    const[products,dispatch]=useReducer(reducer,initialState)
     return (
         <ProductsContext.Provider value={products}>
-            <ProductsContextDispacher.Provider value={setProducts}>
+            <ProductsContextDispacher.Provider value={dispatch}>
                 {children}
             </ProductsContextDispacher.Provider>
         </ProductsContext.Provider>
@@ -20,52 +70,6 @@ const ProductsProvider = ({children}) => {
 export default ProductsProvider;
 export const  useProdocts=()=>useContext(ProductsContext);
 export const UseProductsAction=()=>{
-const setProducts=useContext(ProductsContextDispacher);
-const products=useProdocts();
-const oncrementHandler=(id)=>{
-    const index=products.findIndex((p)=>p.id===id);
-    
-    const product={...products[index]};
-    product.quantity++;
-  const updateProducts=[...products];
- updateProducts[index]=product;
-setProducts(updateProducts);
-
-
-
-}
-const  ondecrementHandler=(id)=>{
-    const index=products.findIndex((item)=>item.id===id);
-    const product={...products[index]};
-       if (product.quantity===1) {
-           const filterItem=products.filter((p)=>p.id!==id)
-           setProducts(filterItem);
-       
-       }
-       else{
-       
-        const updateProducts=[...products];
-    product.quantity--;
-  
- updateProducts[index]=product;
-setProducts(updateProducts)
-       }
-        
-}
- const changeHandler=(event,id)=>{
-    const index=products.findIndex((item)=>item.id===id);
-    const product={...products[index]};
-    product.title=event.target.value;
-   
-    const  updateProducts=[...products];
-    updateProducts[index]=product;
-    setProducts(updateProducts);
-}
-const removeHandler=(id)=>{
-    console.log(id);
-    const filterItem=products.filter((p)=>p.id!==id);
-    setProducts(filterItem);
-}
-return {removeHandler,changeHandler,oncrementHandler,ondecrementHandler}
+return  useContext(ProductsContextDispacher);
 }
 
